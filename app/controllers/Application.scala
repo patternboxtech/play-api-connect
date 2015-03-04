39bb4727e3e207d49f7bb32a34d1e71a9fe35758
Khai Dinh <khai@patternboxtech.com>
2015-03-04 09:10:58 -0500

    Interface with WalkScore API to get walk or transit scores

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -1,12 +1,36 @@
 package controllers
 
-import play.api._
-import play.api.mvc._
+import play.api.Logger
+import play.api.Play.current
+import play.api.libs.concurrent.Execution.Implicits.defaultContext
+import play.api.libs.ws.WS
+import play.api.libs.ws.WSRequestHolder
+import play.api.mvc.Action
+import play.api.mvc.Controller
+import play.api.libs.json.JsObject
 
 object Application extends Controller {
 
   def index = Action {
     Ok(views.html.index("Your new application is ready."))
   }
-
+  
+  def walkScoreApi(url: String) = Action.async(parse.json) { implicit request =>
+    
+    request.body match {
+      case JsObject(fields) => 
+        val queryValues = fields.map { f =>
+          (f._1, f._2.toString())
+        }
+        
+        val holder: WSRequestHolder = WS.url(url).withQueryString(queryValues: _*)
+        
+        Logger.debug("query strings: " + holder.queryString);
+        
+        holder.get().map( response => Ok(response.body) )
+      case _ =>
+        Logger.error("request.body error: " + request.body)
+        scala.concurrent.Future { BadRequest("Expecting application/json request body") }
+    }
+  }
 }
